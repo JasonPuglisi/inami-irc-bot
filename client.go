3cf759a5fa3f9821f3edb6ec814a786e3fdcceed
Jason Puglisi <jason@puglisi.im>
2016-09-15 18:04:58 -0400

    Added client returning and main loop

--- client.go
+++ client.go
@@ -6,19 +6,30 @@ import (
   "github.com/jasonpuglisi/ircutil"
 )
 
+// main creates a server and user, and establishes a connected. It starts a
+// loop that runs until the client is stopped.
 func main() {
-  server, err := ircutil.CreateServer("irc.rizon.net", 6667, true, "")
+  // Create a server for a connection
+  server, err := ircutil.CreateServer("irc.rizon.net", 6667, true, "");
   if err != nil {
-    fmt.Println(err)
+    fmt.Println(err.Error())
     return
   }
 
+  // Create a user for a connection
   user, err := ircutil.CreateUser("Inami", "inami", "Mahiru Inami", 8)
   if err != nil {
-    fmt.Println(err)
+    fmt.Println(err.Error())
     return
   }
 
-  // Create connection to IRC server
-  ircutil.EstablishConnection(server, user);
+  // Establish a connection using a user and server
+  client, err := ircutil.EstablishConnection(server, user);
+  if err != nil {
+    fmt.Println(err.Error())
+    return
+  }
+
+  // Loop until client is stopped
+  for client.Active {}
 }
