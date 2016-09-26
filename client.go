05504495c446e36215b87194058045a89d1aa07a
Jason Puglisi <jason@puglisi.im>
2016-09-26 18:55:30 -0400

    Add command line flags for config file (not yet impmlemented) and debugging, which just shows raw messages

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "flag"
   "fmt"
 
   "github.com/jasonpuglisi/ircutil"
@@ -12,8 +13,13 @@ import (
 // main requests a server and user which it uses establish a connection. It
 // runs a loop to keep the client alive until it is no longer active.
 func main() {
+  // Set config and debug flags, then parse command line arguments.
+  //configPtr := flag.String("config", "config.json", "configuration file")
+  debugPtr := flag.Bool("debug", false, "debugging mode")
+  flag.Parse()
+
   // Request a server with the specified details.
-  server, err := ircutil.CreateServer("irc.rizon.net", 6697, true, "");
+  server, err := ircutil.CreateServer("irc.rizon.net", 6697, true, "")
   if err != nil {
     fmt.Println(err.Error())
     return
@@ -27,8 +33,8 @@ func main() {
   }
 
   // Establish a connection and get a client using user and server details as
-  // well as an initialization function.
-  client, err := ircutil.EstablishConnection(server, user, Init);
+  // well as an initialization function and debugging setting.
+  client, err := ircutil.EstablishConnection(server, user, Init, *debugPtr)
   if err != nil {
     fmt.Println(err.Error())
     return
@@ -40,5 +46,5 @@ func main() {
 
 // Init is executed after the client it connected and registered to the server.
 func Init(client *ircutil.Client) {
-  ircutil.Join(client, "#snowie", "")
+  //ircutil.Join(client, "#snowie", "")
 }
