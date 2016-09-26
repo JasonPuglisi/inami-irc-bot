29907dde85246b20dc8c1cde878f9b19cd322f7e
Jason Puglisi <jason@puglisi.im>
2016-09-26 18:16:04 -0400

    Add initialization function

--- client.go
+++ client.go
@@ -6,6 +6,9 @@ import (
   "github.com/jasonpuglisi/ircutil"
 )
 
+// TODO: All the static values in here will be replaced with config-loaded
+//       values. This includes server/user info and commands.
+
 // main requests a server and user which it uses establish a connection. It
 // runs a loop to keep the client alive until it is no longer active.
 func main() {
@@ -23,8 +26,9 @@ func main() {
     return
   }
 
-  // Establish a connection and get a client using user and server details.
-  client, err := ircutil.EstablishConnection(server, user);
+  // Establish a connection and get a client using user and server details as
+  // well as an initialization function.
+  client, err := ircutil.EstablishConnection(server, user, Init);
   if err != nil {
     fmt.Println(err.Error())
     return
@@ -32,4 +36,9 @@ func main() {
 
   // Loop until client is no longer active.
   for client.Active {}
+}
+
+// Init is executed after the client it connected and registered to the server.
+func Init(client *ircutil.Client) {
+  ircutil.Join(client, "#snowie", "")
 }
